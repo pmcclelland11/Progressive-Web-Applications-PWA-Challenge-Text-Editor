8b81058156142308466926ef2d89149a234e257f
Patrick McClelland <pmcclelland11@gmail.com>
2023-11-25 22:53:20 -0800

    debugged my code and now have a working PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent the default behavior to avoid the automatic prompt
-    event.preventDefault();
-    // Store the event for later use
-    window.deferredPrompt = event;
-    // Update the UI to notify the user that the app can be installed
-    butInstall.removeAttribute('hidden');
-  });
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Store the triggered events
+  window.deferredPrompt = event;
+
+  // Remove the hidden class from the button.
+  butInstall.classList.toggle("hidden", false);
+});
   
   // Implement a click event handler on the `butInstall` element
   butInstall.addEventListener('click', async () => {
@@ -19,21 +18,15 @@ window.addEventListener('beforeinstallprompt', (event) => {
     }
     // Show the installation prompt
     promptEvent.prompt();
-    // Wait for the user to respond to the prompt
-    const userChoice = await promptEvent.userChoice;
-    if (userChoice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    // Clear the deferred prompt
-    window.deferredPrompt = null;
-    // Hide the install button
-    butInstall.setAttribute('hidden', true);
-  });
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
+});
   
   // Add a handler for the `appinstalled` event
   window.addEventListener('appinstalled', (event) => {
     console.log('App installed:', event);
+    window.deferredPrompt = null;
   });
   
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,54 +8,62 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: './index.html',
         title: "Simple Text Editor",
       }),
+
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'sw.js',
+        swDest: 'src-sw.js',
       }),
+
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Progressive Web App: Simple Text Editor',
         short_name: 'Text Editor',
-        description: 'A simple text editor application',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        start_url: "./",
-        publicPath: "./",
+        description: 'A simple text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
-            src: path.resolve('src/images/icon.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
     ],
+
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
               ],
             },
           },
